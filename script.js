58d026f1857e610ba0e400117c410d4a4ca4c270
GabrielleSaraiva11TIA <161879307+GabrielleSaraiva11TIA@users.noreply.github.com>
2026-09-23 23:22:14 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+async function buscarFilmes() {
+    const resposta = await fetch("http://localhost:6767/")
+    const filmes = await resposta.json()
+
+    const lista = document.getElementById("lista-filmes")
+    lista.innerHTML = ""
+
+    filmes.forEach((filme) => {
+        lista.innerHTML += `
+            <li>
+                <strong>${filme.title}</strong> (${filme.gender}) — ${filme.duration} min — Classificação: ${filme.ageLimit}
+            </li>
+        `
+    })
+}
+
+buscarFilmes()
